f98a6af3f4bb1ea9a4cc39ba54b4d15c1a130810
chandu7717 <chanduudgir@gmail.com>
2025-09-10 15:21:41 +0530

    test branch

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ fastapp = FastAPI()        #Creating the instance of the fast API
 
 models.Base.metadata.create_all(engin)  #Creating the database using the data connection URL
 
-fastapp.include_router(user.router)
+fastapp.include_router(user.router)   #user router data
 fastapp.include_router(login_auth.router)
 fastapp.include_router(blog.router)
 fastapp.include_router(refresh.refresh_router)
