3dbd44cefc474f7394cd8c71bdf924fdd0baf25b
chandu7717 <chanduudgir@gmail.com>
2025-09-10 16:19:21 +0530

    test branch

--- app/authentication/JWT_token.py
+++ app/authentication/JWT_token.py
@@ -9,8 +9,7 @@ from jwt import InvalidTokenError
 logging.basicConfig(level=logging.DEBUG)
 from app.schema import schemas
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-secrete_key = secrets.token_hex(32)
-logging.debug(f"auto generated secrete key {secrete_key} ")
+automated_secrete_key = secrets.token_hex(254)
 ALGORITHM = "HS256"  # Hashing method HMAC-SHA256
 ACCESS_TOKEN_EXPIRE_MINUTES = 30  # Defines how long a token should stay valid
 REFRESH_TOKEN_EXPIRY_DAYS = 10
@@ -28,9 +27,11 @@ REFRESH_TOKEN_EXPIRY_DAYS = 10
 
 def create_access_token(data: dict):
     to_encode = data.copy()
+    logging.debug(f"auto generated secrete key {automated_secrete_key} ")
+
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, automated_secrete_key, algorithm=ALGORITHM)
     return encoded_jwt
 
 
@@ -42,13 +43,13 @@ def create_refresh_token(data: dict):
         "exp": expire,
         "type": "refresh"
     })
-    return jwt.encode(data_copy, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(data_copy, automated_secrete_key, algorithm=ALGORITHM)
 
 
 def verify_token(token: str, credentials_exception=None):
     try:
         logging.debug(f"token = {token}")
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, automated_secrete_key, algorithms=[ALGORITHM])
         logging.debug(f"payload data = {payload}")
         user_id = payload.get(
             "sub")  # JWT payload is just a dictionary after decoding, e.g.: Like{"sub":username,"exp":11212}
@@ -61,7 +62,7 @@ def verify_token(token: str, credentials_exception=None):
 
 def verify_refresh_token(token: str):
     try:
-        decode_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        decode_token = jwt.decode(token, automated_secrete_key, algorithms=[ALGORITHM])
         if decode_token.get("type") != "refresh":
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
         return decode_token
